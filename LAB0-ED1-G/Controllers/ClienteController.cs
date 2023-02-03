b00e0bec2a8878c2fd326f3920578453a4d31531
engelsruiz09 <jaruizc@correo.url.edu.gt>
2023-02-03 13:07:39 -0600

    CRUD

---
+++ LAB0-ED1-G/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB0_ED1_G.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection FC)
+        {
+            try
+            {
+                Nombre = FC["Nombre"];
+                Apellido = FC["Apellido"];
+                Telefono = FC["Telefono"];
+                Descripcion = FC["Descripcion"];
+            }
+            catch 
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- LAB0-ED1-G/Models/Cliente.cs
+++ LAB0-ED1-G/Models/Cliente.cs
@@ -1,10 +1,24 @@
-﻿namespace LAB0_ED1_G.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LAB0_ED1_G.Models
 {
     public class Cliente
     {
+        [Display(Name = "Nombre")]
+        [Required]
         public string Nombre { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required]
         public string Apellido { get; set; }
+
+        [Display(Name = "Numero Telefonico")]
+        [Required]
         public int? telefono { get; set; } //el ? significa que los objetos son de tipo anulables
+
+        [Display(Name = "Descripcion del Cliente")]
         public string Descripcion { get; set; }
 
         public void OrdenamientoN(List<Cliente> listaclientes)//aqui va la lista)
@@ -12,7 +26,7 @@
             Cliente aux = new Cliente();
             for(int i = 0; i < listaclientes.Count(); i++)
             {
-                for (int j = 0; i < listaclientes.Count - i - 1; j++)
+                for (int j = 0; i < listaclientes.Count() - i - 1; j++)
                 {
                     if (listaclientes[j].Nombre[0] > listaclientes[j + 1].Nombre[0])
                     {
@@ -29,7 +43,7 @@
             Cliente aux = new Cliente();
             for (int i = 0; i < listaclientes.Count(); i++)
             {
-                for (int j = 0; i < listaclientes.Count - i - 1; j++)
+                for (int j = 0; i < listaclientes.Count() - i - 1; j++)
                 {
                     if (listaclientes[j].Nombre[0] > listaclientes[j + 1].Nombre[0])
                     {
