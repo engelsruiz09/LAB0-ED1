94e10d2f194b5e4cd108c56a162909a49fb12c88
engelsruiz09 <jaruizc@correo.url.edu.gt>
2023-02-04 15:23:21 -0600

    carga media

--- LAB0-ED1-G/Controllers/ClienteController.cs
+++ LAB0-ED1-G/Controllers/ClienteController.cs
@@ -1,34 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LAB0_ED1_G.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
 
 namespace LAB0_ED1_G.Controllers
 {
     public class ClienteController : Controller
     {
-        public IActionResult Index()
+        //GET: 
+        public ActionResult Index()
         {
-            return View();
+            return View(Singleton.Instance.ClienteList);
         }
-
+        //GET:clientecontroller/create
         public ActionResult Create()
         {
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Create(FormCollection FC)
-        //{
-        //    try
-        //    {
-        //        Nombre = FC["Nombre"];
-        //        Apellido = FC["Apellido"];
-        //        Telefono = FC["Telefono"];
-        //        Descripcion = FC["Descripcion"];
-        //    }
-        //    catch 
-        //    {
+        [HttpPost]
+        public ActionResult Create(FormCollection FC) //aqui va a recibir los campos para crear al nuevo cliente
+        {
+            try
+            {
+                var elnuevocliente = new Models.Cliente 
+                {
+                    Nombre = FC["Nombre"],
+                    Apellido = FC["Apellido"],
+                    telefono = Convert.ToInt32(FC["Telefono"]),
+                    Descripcion = FC["Descripcion"]
+                };
+                Singleton.Instance.ClienteList.Add(elnuevocliente);
+                return RedirectToAction(nameof(Index)); //construye una URL de redireccionamiento a una acción/controlador específico en su aplicación y utiliza la tabla de rutas para generar la URL correcta.
 
-        //        throw;
-        //    }
-        //}
+            }
+            catch
+            {
+
+                return View();
+            }
+        }
     }
 }
--- LAB0-ED1-G/Models/Singleton.cs
+++ LAB0-ED1-G/Models/Singleton.cs
@@ -9,18 +9,29 @@ namespace LAB0_ED1_G.Models
     {
         private readonly static Singleton _instance = new Singleton();
         public List<Cliente> ClienteList;
+        private readonly static Singleton _instanceorde = new Singleton();
+        public Cliente Sort;
 
         private Singleton()
         {
-            ClienteList = new List<Cliente>();
+            ClienteList = new List<Cliente>(); //sean crea los objetos
+            Sort = new Cliente();
         }
 
-        //public static Singleton Instance
-        //{
-        //    get
-        //    {
-        //        return_ instance;
-        //    }
-        //}
+        public static Singleton Instance
+        {
+            get
+            {
+                return  _instance;
+            }
+        }
+
+        public static Singleton Instance2
+        {
+            get
+            {
+                return _instanceorde;
+            }
+        }
     }
 }
