9dd01be298df19be68df53629bd30a640c50d426
Emilio Alfaro <h.emilio.alfaro2002@gmail.com>
2023-02-03 20:21:58 -0600

    Clase Singleton

---
+++ LAB0-ED1-G/Models/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB0_ED1_G.Models
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+        public List<Cliente> ClienteList;
+
+        private Singleton()
+        {
+            ClienteList = new List<Cliente>();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return_ instance;
+            }
+        }
+    }
+}
